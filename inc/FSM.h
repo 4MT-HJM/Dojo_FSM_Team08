d50e3f0f57b49f7542aefb7fe079808b81c63367
jiamingh <jiamingh@nokia-sbell.com>
2017-08-31 16:08:01 +0800

    add action to state

--- inc/FSM.h
+++ inc/FSM.h
@@ -3,6 +3,7 @@
 #include "event.h"
 
 #include <memory>
+#include <iostream>
 
 class ILightState;
 
@@ -17,6 +18,10 @@ public:
     void processEvent(const EEvent);
     std::shared_ptr<ILightState> getCurrentState();
     virtual void transiteState(std::shared_ptr<ILightState>);
+    void turnOn()
+    {
+    	std::cout << "turn on in fsm!\n";
+    }
 
     static std::shared_ptr<ILightState> s_stateOff;
     static std::shared_ptr<ILightState> s_stateOneOn;
--- src/LightState.cc
+++ src/LightState.cc
@@ -1,11 +1,17 @@
 #include "LightState.h"
 #include "event.h"
+#include <iostream>
+
+std::function<void()> action1 = []() { std::cout << "turn on!\n"; };
+//std::function<void()> action2 = &LightSwitchFsm::turnOn;
 
 void LightStateOff::handleEvent(EEvent p_event, std::shared_ptr<ILightState>& p_currentState)
 {
     if(EEvent::EEventTurnRight == p_event)
     {
     	p_currentState = LightSwitchFsm::s_stateOneOn;
+    	action1();
+    	//action2();
     }
 }
 
